df206c87752f2d6297e12831028d400bbc03f3f6
Nadine Zielke <nadine.zielke@endava.com>
2022-10-19 18:59:04 +0200

    Aufgabe A3.2 Erstmal Kreis Klasse erstellt +Sternenkreis Aufgabe erfüllt

---
+++ src/Kreis.java
@@ -0,0 +1,23 @@
+public class Kreis {
+    public static void main(String[] args ){
+
+        String einStern  = "*";
+        String zweiSterne = "**";
+
+
+        System.out.printf( "%7s\n" , zweiSterne );
+        System.out.printf( "%2s %8s\n", einStern, einStern);
+        System.out.printf( "%2s %8s\n" , einStern, einStern );
+        System.out.printf( "%7s\n" , zweiSterne );
+
+
+
+
+
+
+
+
+
+
+    }
+}
